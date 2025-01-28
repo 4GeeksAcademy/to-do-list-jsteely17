6cfeabcff1439a745aa09b763d75bf3154c72af5
jsteel17 <161091774+jsteel17@users.noreply.github.com>
2025-01-28 00:24:50 +0000

    First Commit

---
+++ src/js/components/AddTask.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+//create your first component
+const AddTask = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default AddTask;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,15 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
+import AddTask from "./AddTask";
 
 //create your first component
 const Home = () => {
+	const [header, setHeader] = useState("Hello")
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">ToDo List</h1>
+			<input type="text" onChange={e => setHeader(e.target.value)} value={header} >
+			</input>
+			<button onClick="{addTask}">Add</button>
 		</div>
 	);
 };
